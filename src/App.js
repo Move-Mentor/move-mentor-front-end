3d4bc13729364deda1e6e7ced0817d5a27e545e2
Corina <corina@danitt.com>
2023-07-18 11:03:50 +1000

    footer component - complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <p>
           Welcome to Move Mentor! We're still working on this project. Stay tuned.
         </p>
-      </header>
+      </header> */}
+      <Footer />
     </div>
   );
 }

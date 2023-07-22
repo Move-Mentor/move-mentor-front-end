1987105c54784f345883c33e49290dfd95e49fae
Corina <corina@danitt.com>
2023-07-22 15:03:33 +1000

    removed dead code from app.js file, using MainLayout component to keep footer at bottom, added backup colour to header css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
 import './App.css';
-// import Header from './components/header';
-// import Footer from './components/MainLayout/Footer';
 import MainLayout from './components/MainLayout/Layout';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* <header className="App-header">
-//         <p>
-//           Welcome to Move Mentor! We're still working on this project. Stay tuned.
-//         </p>
-//       </header> */}
-//       <Header />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
 
 const App = () => {
   return (

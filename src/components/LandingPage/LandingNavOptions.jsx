8e1c2c6c9eda3c4ad08210be4665dd87a929bece
Corina <corina@danitt.com>
2023-07-23 14:01:25 +1000

    trying to get the cards to display - still not working yet but committing anyways before break

--- src/components/LandingPage/LandingNavOptions.jsx
+++ src/components/LandingPage/LandingNavOptions.jsx
@@ -8,17 +8,17 @@ function NavOptions() {
   const homePageCards = [
     {
       name: "New Student Register",
-      route: "/students/register",
+      // route: "/students/register",
       image: Image1
     },
     {
       name: "Student Login",
-      route: "/students/login",
+      // route: "/students/login",
       image: Image2
     },
     {
       name: "Teacher Login",
-      route: "/teachers/login",
+      // route: "/teachers/login",
       image: Image3
     },
   ];
@@ -27,7 +27,8 @@ function NavOptions() {
     <Card>
        <p>Yoooooo I'm here</p>
       {homePageCards.map((card, index) => (
-        <Card key={index} name={card.name} route={card.route} image={card.image}/>
+        // <Card key={index} name={card.name} route={card.route} image={card.image}/>
+        <Card key={index} name={card.name} image={card.image}/>
       ))}
      
     </Card>
@@ -35,3 +36,45 @@ function NavOptions() {
 }
 
 export default NavOptions;
+
+
+// import React, { useState } from "react";
+// import { Card, Carousel } from "react-bootstrap";
+// import image1 from '../../images/brassMonkeyExtend.png';
+// import image2 from '../../images/aphroditeVariation.png';
+// import image3 from '../../images/felixSplits.png';
+
+
+// const images = [
+//   { src: image1, name: "Allegra Curl" },
+//   { src: image2, name: "Angel - Glorious"},
+//   { src: image3, name: "Headstand"},
+//   // Add more image objects as needed
+// ];
+
+// const NavOptions = () => {
+//   const [activeIndex, setActiveIndex] = useState(0);
+
+//   const handleCarouselSlide = (selectedIndex, e) => {
+//     setActiveIndex(selectedIndex);
+//   };
+
+//   return (
+//     <Card >
+//       <Carousel
+//         >
+//         {images.map((image, index) => (
+//           <Carousel.Item key={index}>
+//             <img
+//               className="d-block w-100"
+//               src={image.src}
+//               alt={`Slide ${index}`}
+//             />
+//           </Carousel.Item>
+//         ))}
+//       </Carousel>
+//     </Card>
+//   );
+// };
+
+// export default NavOptions;

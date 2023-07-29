619d77e11407731f3696219bda7f77e6c01b555e
Corina <corina@danitt.com>
2023-07-29 19:50:13 +1000

    removed dead code

--- src/pages/Options/OptionsMain.jsx
+++ src/pages/Options/OptionsMain.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import OptionsCards from '../../components/OptionsCards/OptionsCards';
 import '../SingleMove/SingleMovePage.css';
-import AddToClass from '../../components/AddToClass/AddToClass';
 import Footer from '../../components/MainLayout/Footer';
 import Header from '../../components/MainLayout/header';
 import '../../App.css';
 
 
-function OptionsLayout({ userRole }) {
+function OptionsLayout() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Header />
       <OptionsCards />
-      <div style={{textAlign: 'center'}}>
-         { userRole === 'teacher' && <AddToClass /> }
-      </div>
       < Footer />
     </div>
   );

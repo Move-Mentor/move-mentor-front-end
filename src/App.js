1f22a1dfd49ed3c509ee8fba5ca129da90060ab8
Corina <corina@danitt.com>
2023-07-29 17:06:32 +1000

    fixed some routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import './App.css';
-// import Login from './pages/StudentLogin/Login';
-// import Home from './pages/Home';
-// import SingleMovePage from './pages/SingleMove/SingleMovePage';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Options from './pages/Options/Options';
+import Login from './pages/StudentLogin/Login';
+import Home from './pages/Home';
+import SingleMovePage from './pages/SingleMove/SingleMovePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Options from './pages/Options/Options';
 
 const App = () => {
 
 
   return (
     <div>
-      <Options />
-      {/* <BrowserRouter>
-        <Routes> */}
-      {/* index is going to be the default page that's rendered */}
-      {/* <Route index element={<Home />} />
-          <Route path="/Login" element={<Login />} />
+      {/* <Options /> */}
+      <BrowserRouter>
+        <Routes>
+          {/* index is going to be the default page that's rendered */}
+          <Route index element={<Home />} />
+          <Route path="/login-student" element={<Login />} />
           <Route path="/SingleMovePage" element={<SingleMovePage />} />
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </div >
   );
 };

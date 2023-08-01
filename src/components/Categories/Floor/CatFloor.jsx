931988e78f95b23d482e836ed46a3f3322830bd6
Corina <corina@danitt.com>
2023-08-01 20:51:54 +1000

    Floor page DRYed up

--- src/components/Categories/Floor/CatFloor.jsx
+++ src/components/Categories/Floor/CatFloor.jsx
@@ -37,7 +37,7 @@ const cardsData = [
 
 const Card = ({ name, image, route, altTag }) => {
   return (
-    <div className="card" style={{ marginBottom: '15px'}}>
+    <div className="card" style={{ margin: '15px'}}>
       <Link to={route}>
         <img src={image} className="card-img-top" alt={altTag} style={{ maxHeight: '25rem' }} />
         <div style={{ textAlign: 'center' }}>
@@ -50,23 +50,12 @@ const Card = ({ name, image, route, altTag }) => {
 
 const FloorList = () => {
   return (
-    <div className="category-grid">
-      <div className="row">
-        <div className="col-md-4">
-          <Card {...cardsData[0]} />
+    <div className="d-flex flex-wrap justify-content-center">
+      {cardsData.map((cardData, index) => (
+        <div key={index} className="col-md-3">
+          <Card {...cardData} />
         </div>
-        <div className="col-md-4">
-          <Card {...cardsData[1]} />
-        </div>
-        <div className="col-md-4">
-          <Card {...cardsData[2]} />
-        </div> 
-      </div>
-      <div className="row">
-        <div className="col-md-12">
-          <Card {...cardsData[3]} />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };

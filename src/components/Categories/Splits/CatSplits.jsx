86c26f7a7f46ab7c0ba81c26c713167c7e0ac716
Corina <corina@danitt.com>
2023-08-01 21:08:33 +1000

    Splits DRYed up

--- src/components/Categories/Splits/CatSplits.jsx
+++ src/components/Categories/Splits/CatSplits.jsx
@@ -22,7 +22,7 @@ const cardsData = [
 
 const Card = ({ name, image, route, altTag }) => {
   return (
-    <div className="card" style={{ marginBottom: '15px'}}>
+    <div className="card" style={{ margin: '15px'}}>
       <Link to={route}>
         <img src={image} className="card-img-top" alt={altTag} style={{ maxHeight: '25rem' }} />
         <div style={{ textAlign: 'center' }}>
@@ -35,15 +35,12 @@ const Card = ({ name, image, route, altTag }) => {
 
 const SplitsList = () => {
   return (
-    <div className="category-grid">
-      <div className="row">
-        <div className="col-md-6">
-          <Card {...cardsData[0]} />
+    <div className="d-flex flex-wrap justify-content-center">
+      {cardsData.map((cardData, index) => (
+        <div key={index} className="col-md-3">
+          <Card {...cardData} />
         </div>
-        <div className="col-md-6">
-          <Card {...cardsData[1]} />
-        </div>
-      </div> 
+      ))}
     </div>
   );
 };

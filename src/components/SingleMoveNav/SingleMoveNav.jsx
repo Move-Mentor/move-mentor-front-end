329eaf3824318d11c860c3bc2084e93b43e701fb
Corina <corina@danitt.com>
2023-08-03 22:26:04 +1000

    fixed routing from single move page to categories page

--- src/components/SingleMoveNav/SingleMoveNav.jsx
+++ src/components/SingleMoveNav/SingleMoveNav.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import CardGroup from 'react-bootstrap/CardGroup';
-import Card from 'react-bootstrap/Card';
-import '../../App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import CardGroup from "react-bootstrap/CardGroup";
+import Card from "react-bootstrap/Card";
+import "../../App.css";
 
 function SingleMoveNav() {
   return (
     <CardGroup>
-      <Card style={{border: 'none'}}>
-        <Link to={'/CategoriesMain'}>
-          <div className="all-moves-and-classes-nav" variant="link">All Moves</div>
+      <Card style={{ border: "none" }}>
+        <Link to={"/categories"}>
+          <div className="all-moves-and-classes-nav" variant="link">
+            All Moves
+          </div>
         </Link>
       </Card>
-      <Card style={{border: 'none'}}>
-        <Link to={'/Lessons'}>
-          <div className="all-moves-and-classes-nav" variant="link">Classes</div>
+      <Card style={{ border: "none" }}>
+        <Link to={"/Lessons"}>
+          <div className="all-moves-and-classes-nav" variant="link">
+            Classes
+          </div>
         </Link>
       </Card>
     </CardGroup>
-  )}
+  );
+}
 
-  export default SingleMoveNav;
-
-  
+export default SingleMoveNav;

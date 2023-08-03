06d35cda7ef7485a580d9540d5dc7a50c44f20a5
Corina <corina@danitt.com>
2023-08-03 21:54:14 +1000

    styling fix for main categories page

--- src/pages/Categories/CategoriesMain.jsx
+++ src/pages/Categories/CategoriesMain.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import HeaderTop from "../../components/MainLayout/HeaderTop";
 import "../SingleMove/SingleMovePage.css";
 import Footer from "../../components/MainLayout/Footer";
 import "../../App.css";
@@ -8,17 +8,18 @@ import CategoryCard from "../../components/CategoryCard/CategoryCard";
 
 function CategoriesMain() {
   return (
-    <div
-      style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
-    >
-      <div className="d-flex flex-wrap justify-content-center">
-        {categories.map((category, index) => (
-          <div key={index} className="col-md-3">
-            <CategoryCard {...category} />
-          </div>
-        ))}
+    <div>
+      <HeaderTop />
+      <div className="main-div">
+        <div className="d-flex flex-wrap justify-content-center">
+          {categories.map((category, index) => (
+            <div key={index} className="col-md-3">
+              <CategoryCard {...category} />
+            </div>
+          ))}
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }

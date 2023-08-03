ff7c54f2d3f6ac0a4521e085c4e01b1bd4cf296e
Corina <corina@danitt.com>
2023-08-03 22:08:32 +1000

    pseudocode to start bringing in the backend

--- src/pages/SingleMove/SingleMovePage.jsx
+++ src/pages/SingleMove/SingleMovePage.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import HeaderTop from '../../components/MainLayout/HeaderTop';
-import Footer from '../../components/MainLayout/Footer';
-import CarouselComponent from '../../components/Carousel/carousel';
-import './SingleMovePage.css';
-import SingleMoveNav from '../../components/SingleMoveNav/SingleMoveNav';
-import AddToClass from '../../components/AddToClass/AddToClass';
-import '../../App.css';
+import React from "react";
+import HeaderTop from "../../components/MainLayout/HeaderTop";
+import Footer from "../../components/MainLayout/Footer";
+import CarouselComponent from "../../components/Carousel/carousel";
+import "./SingleMovePage.css";
+import SingleMoveNav from "../../components/SingleMoveNav/SingleMoveNav";
+import AddToClass from "../../components/AddToClass/AddToClass";
+import "../../App.css";
 
 function SingleMovePage({ userRole }) {
+  // function SingleMovePage(props) {
+  // const { userRole } = props;
+  // // fetch from API ..
+  // let classes = [];
+  // const classesResponse = fetch('https://api.movementor.com/classes').then(async (response) => {
+  //   classes = await response.json();
+  // })
+  // const classes = [
+  //   {
+  //     id: '',
+  //     name: 'Monday: intro',
+  //     img: 'https://...thing.jpg',
+  //     moves: [
+  //       {
+  //         id: 'flangelina',
+  //         name: 'flangelina'
+  //       }
+  //     ],
+  //   }
+  // ];
   return (
     <div className="main-div">
       <HeaderTop />
-      <div style={{textAlign: 'center' }}>
+      <div style={{ textAlign: "center" }}>
         <SingleMoveNav />
       </div>
       <div className="container mt-4 d-flex justify-content-center">
@@ -19,9 +39,12 @@ function SingleMovePage({ userRole }) {
           <CarouselComponent />
         </div>
       </div>
-      <div style={{textAlign: 'center'}}>
-        { userRole === 'teacher' && <AddToClass /> }
+      <div style={{ textAlign: "center" }}>
+        {userRole === "teacher" && <AddToClass />}
       </div>
+      {/* {classes.map(class => (
+        <ClassCard class={class} />
+      ))} */}
       <Footer />
     </div>
   );

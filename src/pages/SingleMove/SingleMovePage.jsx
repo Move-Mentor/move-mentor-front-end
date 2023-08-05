87dc38a5220ff516e2334df2c71cbccab169c768
Corina <corina@danitt.com>
2023-08-05 20:44:25 +1000

    removed carousel

--- src/pages/SingleMove/SingleMovePage.jsx
+++ src/pages/SingleMove/SingleMovePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HeaderTop from "../../components/MainLayout/HeaderTop";
 import Footer from "../../components/MainLayout/Footer";
-import CarouselComponent from "../../components/Carousel/carousel";
+
 import "./SingleMovePage.css";
 import SingleMoveNav from "../../components/SingleMoveNav/SingleMoveNav";
 import AddToClass from "../../components/AddToClass/AddToClass";
@@ -35,11 +35,7 @@ function SingleMovePage({ userRole }) {
         <div style={{ textAlign: "center" }}>
           <SingleMoveNav />
         </div>
-        <div className="container mt-4 d-flex justify-content-center">
-          <div className="col-md-6 col-lg-6 col-xl-6">
-            <CarouselComponent />
-          </div>
-        </div>
+        <div className="container mt-4 d-flex justify-content-center"></div>
         <div style={{ textAlign: "center" }}>
           {userRole === "teacher" && <AddToClass />}
         </div>

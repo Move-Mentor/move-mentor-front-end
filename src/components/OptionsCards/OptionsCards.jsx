67a18aa97b9d308f62b2248ca9be47de3ddf84c0
KarlaB3 <k.bucoy@gmail.com>
2023-08-06 07:07:42 +1000

    feat: student classmoves link displays their specific lesson moves

--- src/components/OptionsCards/OptionsCards.jsx
+++ src/components/OptionsCards/OptionsCards.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import "../../App.css";
-
+import { useLessonContext } from "../../contexts/LessonContext";
 const studentCardsData = [
   {
     name: "Lesson Moves",
     image:
       "https://movementor.s3.ap-southeast-2.amazonaws.com/OptionsNavigation/optionsClassMoves.png",
-    route: "/lessonmoves",
+    route: "/lessons/:id",
     color: "#bcd5cf",
     altTag: "Lesson Moves",
   },
@@ -76,17 +76,29 @@ const Card = ({ name, image, route, color, altTag }) => {
 const OptionsNav = ({ role }) => {
   const cardsData = role === "student" ? studentCardsData : teacherCardsData;
 
+  // Get the lesson ID from the lessons data
+  const { lessons } = useLessonContext();
+
   return (
     <div style={{ marginTop: "8rem" }}>
       <div className="d-flex flex-wrap justify-content-center">
-        {cardsData.map((cardData, index) => (
-          <div key={index} className="col-md-3">
-            <Card {...cardData} />
-          </div>
-        ))}
+        {cardsData.map((cardData, index) => {
+          // For the "Lesson Moves" card, dynamically update the route with the lesson id
+          if (cardData.name === "Lesson Moves" && lessons.length > 0) {
+            const lessonId = lessons[0]._id;
+            cardData.route = `/lessons/${lessonId}`;
+          }
+
+          return (
+            <div key={index} className="col-md-3">
+              <Card {...cardData} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 };
+
 
 export default OptionsNav;

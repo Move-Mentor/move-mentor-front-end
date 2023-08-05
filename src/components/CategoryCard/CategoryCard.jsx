f3208992067db5f046ff1aefda2c7b193c59a804
Corina <corina@danitt.com>
2023-08-06 09:16:51 +1000

    removed dead CSS file

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,43 +1,43 @@
 import { Link } from "react-router-dom";
-import "./CategoryCard.css";
+// import "./CategoryCard.css";
 
-const CategoryCard = ({ category }) => {  
+const CategoryCard = ({ category }) => {
   console.log("Category prop:", category);
   // Define category image url and alt tag mappings
   const categoryImageUrls = {
-    "Inverts": {
+    Inverts: {
       url: "https://movementor.s3.ap-southeast-2.amazonaws.com/Inverts/invertsInvertStraddle.png",
-      alt: "Inverts"
+      alt: "Inverts",
     },
-    "Sits": {
+    Sits: {
       url: "https://movementor.s3.ap-southeast-2.amazonaws.com/Sits/sitsMartini.png",
-      alt: "Sits"
+      alt: "Sits",
     },
-    "Spins": {
+    Spins: {
       url: "https://movementor.s3.ap-southeast-2.amazonaws.com/Spins/spinsBackwardsSunwheel.png",
-      alt: "Spins"
+      alt: "Spins",
     },
-    "Floor": {
+    Floor: {
       url: "https://movementor.s3.ap-southeast-2.amazonaws.com/Floor/floorBodyRolls.png",
-      alt: "Floor"
+      alt: "Floor",
     },
-    "Aerials": {
+    Aerials: {
       url: "https://movementor.s3.ap-southeast-2.amazonaws.com/Aerials/aerialsAerialInvert.png",
-      alt: "Aerials"
+      alt: "Aerials",
     },
-    "Climbs": {
+    Climbs: {
       url: "https://movementor.s3.ap-southeast-2.amazonaws.com/Climbs/climbsAngelBasic.png",
-      alt: "Climbs"
+      alt: "Climbs",
     },
-    "Splits": {
+    Splits: {
       url: "https://movementor.s3.ap-southeast-2.amazonaws.com/Splits/splitsBrassSplit.png",
-      alt: "Splits"
+      alt: "Splits",
     },
-  }
-  
+  };
+
   const categoryData = categoryImageUrls[category];
   console.log("Category data:", categoryData);
-  const { url, alt } = categoryData
+  const { url, alt } = categoryData;
 
   return (
     <div className="card" style={{ margin: "15px" }}>
@@ -48,7 +48,7 @@ const CategoryCard = ({ category }) => {
           alt={alt}
           style={{ maxHeight: "25rem" }}
         />
-        <div style={{ backgroundColor: '#bcd5cf', textAlign: "center" }}>
+        <div style={{ backgroundColor: "#bcd5cf", textAlign: "center" }}>
           {category}
         </div>
       </Link>

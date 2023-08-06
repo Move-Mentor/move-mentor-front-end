07639f91f0a0a04601b4a7735d385fdd6964a65c
Corina <corina@danitt.com>
2023-08-06 15:45:05 +1000

    revert CategoriesDetail changes

--- src/pages/Categories/CategoriesDetail.jsx
+++ src/pages/Categories/CategoriesDetail.jsx
@@ -1,99 +1,71 @@
-//import { useEffect, useState } from "react";
-//import axios from "axios";
-//import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import HeaderTop from "../../components/MainLayout/HeaderTop";
 import Footer from "../../components/MainLayout/Footer";
-<<<<<<< HEAD
-//import { useToken } from "../../contexts/TokenContext";
-// import Accord from "../../components/AddToClass/AddToClass";
-=======
 import { useToken } from "../../contexts/TokenContext";
 import Accord from "../../components/AddToClass/AddToClass";
 import CategoryNav from "../../components/CategoryNav/CategoryNav";
->>>>>>> a00577bea539bd5a929a667a048f33dbbcc83f90
 
-// const CategoryMoveCard = ({ move, role }) => {
-//   return (
-//     <div className="card" style={{ margin: "15px" }}>
-//       <img
-//         src={move.moveImage}
-//         className="card-img-top"
-//         alt={move.moveName}
-//         style={{ maxHeight: "25rem" }}
-//       />
-//       <div style={{ textAlign: "center" }}>{move.moveName}</div>
-//       {/* Teacher Actions */}
-//       {role === "teacher" && (
-//         <div>
-//           <Accord />
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
+const CategoryMoveCard = ({ move, role }) => {
+  return (
+    <div className="card" style={{ margin: "15px" }}>
+      <img
+        src={move.moveImage}
+        className="card-img-top"
+        alt={move.moveName}
+        style={{ maxHeight: "25rem" }}
+      />
+      <div style={{ textAlign: "center" }}>{move.moveName}</div>
+      {/* Teacher Actions */}
+      {role === "teacher" && (
+        <div>
+          <Accord />
+        </div>
+      )}
+    </div>
+  );
+};
 
-// const api = process.env.REACT_APP_DATABASE_URL;
+const api = process.env.REACT_APP_DATABASE_URL;
 
-// // Function to fetch categories and display their associated moves
-// export const CategoriesDetail = () => {
-//   const { category } = useParams();
-//   const [categoryData, setCategoryData] = useState([]);
+// Function to fetch categories and display their associated moves
+export const CategoriesDetail = () => {
+  const { category } = useParams();
+  const [categoryData, setCategoryData] = useState([]);
 
-//   const { role } = useToken();
-//   console.info("role", role);
+  const { role } = useToken();
+  console.info("role", role);
 
-//   useEffect(() => {
-//     axios
-//       .get(`${api}/moves/categories/${encodeURIComponent(category)}`)
-//       .then((response) => {
-//         setCategoryData(response.data);
-//         console.log("category moves", response.data); // log received data in the console for testing
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching category data:", error);
-//         setCategoryData([]);
-//       });
-//   }, [category]);
+  useEffect(() => {
+    axios
+      .get(`${api}/moves/categories/${encodeURIComponent(category)}`)
+      .then((response) => {
+        setCategoryData(response.data);
+        console.log("category moves", response.data); // log received data in the console for testing
+      })
+      .catch((error) => {
+        console.error("Error fetching category data:", error);
+        setCategoryData([]);
+      });
+  }, [category]);
 
-//   return (
-//     <div>
-//       <HeaderTop />
-//       {categoryData && (
-//         <div className="d-flex flex-wrap justify-content-center">
-//           {categoryData.map((move) => (
-//             <div className="col-md-3" key={move._id}>
-//               <CategoryMoveCard move={move} role={role} />
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default CategoriesDetail;
-
-import "../../App.css";
-import CategoryMovesFetch from "../../components/CategoryCard/CategoryMovesFetch";
-
-function CategoriesDetail() {
   return (
     <div>
       <HeaderTop />
-<<<<<<< HEAD
-      <div className="main-div">
-=======
       <CategoryNav />
       {categoryData && (
->>>>>>> a00577bea539bd5a929a667a048f33dbbcc83f90
         <div className="d-flex flex-wrap justify-content-center">
-          <CategoryMovesFetch />
+          {categoryData.map((move) => (
+            <div className="col-md-3" key={move._id}>
+              <CategoryMoveCard move={move} role={role} />
+            </div>
+          ))}
         </div>
-        <Footer />
-      </div>
+      )}
+      <Footer />
     </div>
   );
-}
+};
 
 export default CategoriesDetail;

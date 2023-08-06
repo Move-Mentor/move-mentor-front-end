000cb27234e51488f16b688f967468619ee9b9ab
Corina <corina@danitt.com>
2023-08-06 20:41:54 +1000

    moves now appear inside lessons

--- src/pages/Lessons/LessonsDetail.jsx
+++ src/pages/Lessons/LessonsDetail.jsx
@@ -1,46 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import HeaderTop from "../../components/MainLayout/HeaderTop";
 import Footer from "../../components/MainLayout/Footer";
 import InnerPageNav from "../../components/InnerPageNav/InnerPageNav";
-import { Link } from "react-router-dom";
-import { lessons } from "../../lessons";
-
-import { useParams } from "react-router-dom";
-
-const Card = ({ name, image, route, altTag }) => {
-  return (
-    <div className="card" style={{ margin: "15px" }}>
-      <Link className="nav-link btn" to={route}>
-        <img
-          src={image}
-          className="card-img-top"
-          alt={altTag}
-          style={{ maxHeight: "25rem" }}
-        />
-        <div style={{ textAlign: "center" }}>{name}</div>
-      </Link>
-    </div>
-  );
-};
+import { useLessonContext } from "../../contexts/LessonContext";
+import CategoryMoveCard from "../../components/CategoryCard/CategoryMoveCard";
 
 function LessonsDetail() {
+  const [moves, setMoves] = useState([]);
   let params = useParams();
-  console.log(params.name);
-  const lesson = lessons.find(function (less) {
-    return less.id === params.name;
+  const { lessons, fetchLessonMoves } = useLessonContext();
+  const lesson = lessons.find(function (l) {
+    return l._id === params.id;
   });
-  console.log(lesson);
+
+  useEffect(() => {
+    if (!lesson) {
+      return;
+    }
+    fetchLessonMoves(lesson).then((fetchedMoves) => {
+      setMoves(fetchedMoves);
+    });
+  }, [lesson, fetchLessonMoves]);
 
   return (
     <div>
       <HeaderTop />
       <InnerPageNav />
       <div className="d-flex flex-wrap justify-content-center">
-        {lesson.moves.map((move, index) => (
-          <div key={index} className="col-md-3">
-            <Card {...move} />
-          </div>
-        ))}
+        {moves.length ? (
+          moves.map((move) => (
+            <div key={move._id} className="col-md-3">
+              <CategoryMoveCard move={move} />
+            </div>
+          ))
+        ) : (
+          <p>Loading Lesson Moves..</p>
+        )}
       </div>
       <Footer />
     </div>
--- src/pages/Lessons/LessonsMain.jsx
+++ src/pages/Lessons/LessonsMain.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import HeaderTop from "../../components/MainLayout/HeaderTop";
 import Footer from "../../components/MainLayout/Footer";
+import { useLessonContext } from "../../contexts/LessonContext";
+import LessonCard from "../../components/Lessons/LessonCard";
+
 import "../../App.css";
-import LessonFetch from "../../components/Lessons/LessonFetch";
 
 function LessonsMain() {
+  const { lessons } = useLessonContext();
+
   return (
     <div>
       <HeaderTop />
       <div className="main-div">
         <div className="d-flex flex-wrap justify-content-center">
-            <div className="col-md-3">
-              <LessonFetch/>
-            </div>
+          {lessons.length === 0 ? (
+            <p>No lessons available.</p>
+          ) : (
+            lessons.map((lesson) => (
+              <div className="col-md-3" key={lesson._id}>
+                <LessonCard lesson={lesson} />
+              </div>
+            ))
+          )}
         </div>
         <Footer />
       </div>

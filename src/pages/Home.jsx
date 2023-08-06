2b5239bea437323df55a740f59e53c1613fa359b
Corina <corina@danitt.com>
2023-08-06 11:27:22 +1000

    homepage now has in-line styling as that isn't used anywhere else

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import '../../src/App.css';
-import HomeMainImage from '../components/HomePage/HomeMainImage';
-import HomeNavOptions from '../components/HomePage/HomeNavOptions';
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
+import React from "react";
+import HomeMainImage from "../components/HomePage/HomeMainImage";
+import HomeNavOptions from "../components/HomePage/HomeNavOptions";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import "../App.css";
 
 function HomeLayout() {
   return (
     <div className="main-body-layout">
-      <CardGroup >
-        <Card className="left-side-image-card">
+      <CardGroup>
+        <Card style={{ backgroundColor: "#f1daae", border: "none" }}>
           <Card.Body>
             <HomeMainImage />
           </Card.Body>
         </Card>
-        <Card style={{border: 'none'}}>
+        <Card style={{ border: "none" }}>
           <Card.Body>
             <HomeNavOptions />
           </Card.Body>
         </Card>
       </CardGroup>
       {/* Empty div with "auto" height to push the cards to the bottom */}
-      <div style={{ height: 'auto' }}></div>
+      <div style={{ height: "auto" }}></div>
     </div>
   );
 }

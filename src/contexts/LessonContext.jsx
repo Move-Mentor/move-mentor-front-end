ff048694ea83f3230a4b105704486c187110d478
Corina <corina@danitt.com>
2023-08-06 16:13:24 +1000

    fetch lessons in context

--- src/contexts/LessonContext.jsx
+++ src/contexts/LessonContext.jsx
@@ -1,4 +1,8 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
+import { useToken } from "./TokenContext";
+
+const api = process.env.REACT_APP_DATABASE_URL;
 
 const LessonContext = createContext();
 
@@ -8,10 +12,38 @@ export const useLessonContext = () => {
 
 export const LessonProvider = ({ children }) => {
   const [lessons, setLessons] = useState([]);
+  const { token, role } = useToken();
+
+  useEffect(() => {
+    // Re-fetch lessons when role is changed
+    if (!token || !role) {
+      // user not yet authenticated, do not yet load lessons
+      return;
+    }
+    fetchLessons(token, role).then((lessons) => {
+      setLessons(lessons);
+    });
+  }, [role, token]);
 
   return (
-    <LessonContext.Provider value={{ lessons, setLessons }}>
+    <LessonContext.Provider value={{ lessons }}>
       {children}
     </LessonContext.Provider>
   );
 };
+
+async function fetchLessons(token, role) {
+  try {
+    // Create a configuration object with the authorization header containing the JWT
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const response = await axios.get(`${api}/users/profile/${role}`, config);
+    return response.data.lessons;
+  } catch (error) {
+    console.error(error);
+  }
+}
